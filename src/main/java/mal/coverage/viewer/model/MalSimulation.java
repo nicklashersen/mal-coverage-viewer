385dd3eed1c8a7f82c2f837bd77996a91d346181
Nicklas Hersén <nicklas.hersen@gmail.com>
2020-08-22 18:58:59 +0200

    Render defense state

--- src/main/java/mal/coverage/viewer/model/MalSimulation.java
+++ src/main/java/mal/coverage/viewer/model/MalSimulation.java
@@ -7,12 +7,12 @@ public class MalSimulation {
 	public final String name;
 	public final String className;
 	public final Map<Integer, Double> compromisedAttackSteps; // (id, ttc)
-	public final Set<Integer> compromisedDefenses; // id
+	public final Set<Integer> activeDefenses; // id
 
 	public MalSimulation(String name, String className, Map<Integer, Double> steps, Set<Integer> defenses) {
 		this.name = name;
 		this.className = className;
 		compromisedAttackSteps = steps;
-		compromisedDefenses = defenses;
+		activeDefenses = defenses;
 	}
 }
